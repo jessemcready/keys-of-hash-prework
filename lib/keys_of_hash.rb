d015ef4a852ced2ca32aeca66870fac01f8ff354
Jesse McReady <jessemcready@gmail.com>
2018-06-28 20:26:23 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    arguments.each do |key, value|
-      puts "#{key} : #{value}"
+    arguments.each do |key|
+      puts "#{key}"
     end
   end
 end
